d06e9cb95c002aa0723b4e202d7b6a460c4e6773
Issra Hashim <issrahashim@hotmail.fr>
2021-10-12 19:39:28 +0100

    special food added and score updated

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SEI-Project-One
 
 // challenging to get pacman to stop at walls, ended up using classes to prevent the movement
+// making enemmies move was a challenge too, so broke down the problem by writing one enemy in an object and writing a function similar to that of keydown for player
+// when this worked, i tried making this function reusable so i could pass all the ghosts as arguments and use the same function
+// after making all enemies move, i still have to refine the enemy movement so they move at random rather than follow a set if / else if statement
--- scripts/app.js
+++ scripts/app.js
@@ -37,6 +37,7 @@ function init() {
   const currentScore = document.querySelector('span')
   let score = 0
   const innerWalls = ['32', '34', '35', '37', '52', '53', '53', '56', '57','82', '87', '92','97', '102', '103', '106', '107', '112', '117', '143', '146', '153' ,'156', '163', '164', '165', '166', '192', '197', '202', '203', '206', '207', '212', '217', '222', '227', '252', '253', '256', '257', '272', '274', '275', '277' ]
+  const specialFood = ['41', '48', '105', '151', '158', '205', '261', '268']
   const startingPacmanPosition = '255'
   let currentPacmanPosition = 255
   const pacmanPreviousPositions= []
@@ -79,6 +80,7 @@ function init() {
   // loop 
   // make the choice, then check if they run into wall or run into another ghost, or position they've been in before
     
+
   function createGrid() {
     for (let i = 0; i < 300; i++) {
       const cell = document.createElement('div')
@@ -91,8 +93,10 @@ function init() {
       if (space.innerText % width === 0 || space.innerText % width === width - 1 || space.innerText < width || space.innerText > width * (width + width + width) - 10 || innerWalls.includes(space.innerText))  {
         space.classList.add('walls')
         walls.push(space)
-      } else if (!startingGhostPosition.includes(space.innerText) && space.innerText !== startingPacmanPosition) {
+      } else if (startingGhostPosition.includes(space.innerText) === false && space.innerText !== startingPacmanPosition && specialFood.includes(space.innerText) === false) {
         space.classList.add('food')
+      } else if (specialFood.includes(space.innerText)){
+        space.classList.add('special')
       } else if (startingGhostPosition.includes(space.innerText))
         space.classList.add('ghost')
     })
@@ -135,6 +139,10 @@ function init() {
         clearInterval(myInterval)
         gameOver()
       }
+      // if (cells[pacman].classList.contains('specialmode') && cells[ghost.position].classList.contains('pacman')){
+      //   clearInterval(myInterval)
+      //   gameOver()
+      // }
     }, ghost.speed)
   }
 
@@ -180,6 +188,7 @@ function init() {
 
   function handleKeyDown(event) {
     const key = event.keyCode
+    console.log(event.ke)
     removePacman(currentPacmanPosition)
     if (key === 39 && cells[currentPacmanPosition + 1 ].classList.contains('walls') === false && cells[currentPacmanPosition].classList.contains('ghost') === false) {
       currentPacmanPosition++
@@ -192,16 +201,40 @@ function init() {
     }
     addPacman(currentPacmanPosition)
     pacmanPreviousPositions.push(currentPacmanPosition)
-    if (cells[currentPacmanPosition].classList.contains('food')){
+    if (cells[currentPacmanPosition].classList.contains('food') || cells[currentPacmanPosition].classList.contains('special')){
       score++
       cells[currentPacmanPosition].classList.remove('food')
+      cells[currentPacmanPosition].classList.remove('special')
       currentScore.innerText = score
-    }  
+    } 
     if (cells[currentPacmanPosition].classList.contains('ghost')){
       gameOver()
     }
   }
 
+  // let counter
+  // function specialMode(event){
+  //   myInterval = setInterval(() => {
+  //     counter++
+  //     const key = event.keycode
+  //     if (counter > 10){
+  //       clearInterval(myInterval)
+  //     } 
+  //       if (key === 39 && cells[currentPacmanPosition + 1 ].classList.contains('walls') === false) {
+  //         currentPacmanPosition++
+  //       } else if (key === 37 && cells[currentPacmanPosition - 1 ].classList.contains('walls') === false) {
+  //         currentPacmanPosition--
+  //       } else if (key === 38 && cells[currentPacmanPosition - width ].classList.contains('walls') === false) {
+  //         currentPacmanPosition -= width
+  //       } else if (key === 40 && cells[currentPacmanPosition + width ].classList.contains('walls') === false) {
+  //         currentPacmanPosition += width
+  //       }
+  //       if (cells[currentPacmanPosition].classList.contains('ghost')){
+  //         score++
+  //         currentScore.innerText = score
+  //       }
+      
+  //   })
 
   document.addEventListener('keydown', handleKeyDown)
 
@@ -212,7 +245,6 @@ function init() {
 
 }
 window.addEventListener('DOMContentLoaded', init)
-
 // myInterval = setInterval(()=> {
 //   removeGhost(currentGhostPosition)
 //   currentGhostPosition.forEach(ghost => {
